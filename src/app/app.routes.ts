68a07d902a2cd9527e2313783c8ee213a03e433b
atsneto <toscanosorrentinonetoa@gmail.com>
2024-11-04 19:33:46 -0300

    Página notas e frequência aluno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {LoginComponent} from './page/login/login.component';
 import {HomeAlunoComponent} from './page/home-aluno/home-aluno.component';
 import {RedefinirSenhaComponent} from './page/redefinir-senha/redefinir-senha.component';
 import {UcConteudoComponent} from './page/uc-conteudo/uc-conteudo.component';
+import {AlunoNotasFrequenciaComponent} from './page/aluno-notas-frequencia/aluno-notas-frequencia.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'home-aluno', component: HomeAlunoComponent},
   {path: 'redefinir-senha', component: RedefinirSenhaComponent},
-  {path: 'uc-conteudo', component:UcConteudoComponent}
+  {path: 'uc-conteudo', component:UcConteudoComponent},
+  {path: 'aluno-notas-frequencia', component: AlunoNotasFrequenciaComponent}
 ];
