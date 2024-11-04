09def661acde9d0f72f76417a180cb45753c05ee
atsneto <toscanosorrentinonetoa@gmail.com>
2024-11-04 19:22:56 -0300

    Balões notas e frequência

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from './page/login/login.component';
 import {HomeAlunoComponent} from './page/home-aluno/home-aluno.component';
 import {RedefinirSenhaComponent} from './page/redefinir-senha/redefinir-senha.component';
+import {UcNotasFrequenciaComponent} from './page/uc-notas-frequencia/uc-notas-frequencia.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'home-aluno', component: HomeAlunoComponent},
-  {path: 'redefinir-senha', component: RedefinirSenhaComponent}
+  {path: 'redefinir-senha', component: RedefinirSenhaComponent},
+  {path: 'uc-notas-frequencia', component: UcNotasFrequenciaComponent},
 ];
